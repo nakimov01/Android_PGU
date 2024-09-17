5c57c5325f8be9616a10361b835a87c748f67b5f
nakimov01 <nikita2003akimov@bk.ru>
2024-09-17 03:30:18 +0300

    Лабараторная №1 90% Добавлены все основные элементы: -TextView -Кнопки -Toastы

--- app/src/main/java/com/example/quest/MainActivity.java
+++ app/src/main/java/com/example/quest/MainActivity.java
@@ -4,39 +4,52 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
 import android.widget.Toast;
+import com.example.quest.R;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button mTrueButton;
-    private Button mFalseButton;
+    private Button developer1;
+    private Button developer2;
+    private Button developer3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTrueButton = (Button)findViewById(R.id.button1);
+        developer1 = (Button)findViewById(R.id.developerNikita);
 
-        // Назначение слушателя для кнопки «Да»
-        mTrueButton.setOnClickListener(new View.OnClickListener() {
+
+        developer1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this,
-                        R.string.incorrect_toast,
+                        R.string.developer1,
                         Toast.LENGTH_SHORT).show();
             }
         });
 
 
-        mFalseButton = (Button) findViewById(R.id.button9);
+        developer2 = (Button) findViewById(R.id.developerSasha);
 
-        // Назначение слушателя для кнопки «Нет»
-        mFalseButton.setOnClickListener(new View.OnClickListener() {
+
+        developer2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this,
-                        R.string.correct_toast,
+                        R.string.developer2,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        developer3 = (Button) findViewById(R.id.developerMax);
+
+        developer3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,
+                        R.string.developer3,
                         Toast.LENGTH_SHORT).show();
             }
         });
