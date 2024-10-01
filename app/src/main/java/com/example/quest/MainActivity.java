2060fb4b70c2a19f03ba13684901edb207b9ea91
nakimov01 <nikita2003akimov@bk.ru>
2024-10-01 06:51:33 +0300

    Лабараторная №1-2 35% Начало добавления логики

--- app/src/main/java/com/example/quest/MainActivity.java
+++ app/src/main/java/com/example/quest/MainActivity.java
@@ -3,57 +3,67 @@ package com.example.quest;
 import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 import com.example.quest.R;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button developer1;
-    private Button developer2;
-    private Button developer3;
+    private Button nikitaBtn;
+    private Button maxBtn;
+    private Button sashaBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        developer1 = (Button)findViewById(R.id.developerNikita);
+        nikitaBtn = (Button)findViewById(R.id.developerNikita);
+        maxBtn = (Button) findViewById(R.id.developerSasha);
+        sashaBtn = (Button) findViewById(R.id.developerMax);
 
 
-        developer1.setOnClickListener(new View.OnClickListener() {
+        nikitaBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this,
                         R.string.developer1,
                         Toast.LENGTH_SHORT).show();
+
+                nikitaBtn.setBackgroundResource(R.drawable.round_green_stroke);
+                maxBtn.setBackgroundResource(R.drawable.white_button_background);
+                sashaBtn.setBackgroundResource(R.drawable.white_button_background);
+                nikitaBtn.setBackgroundTintList(null);
+
             }
         });
 
-
-        developer2 = (Button) findViewById(R.id.developerSasha);
-
-
-        developer2.setOnClickListener(new View.OnClickListener() {
+        maxBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this,
                         R.string.developer2,
                         Toast.LENGTH_SHORT).show();
+                maxBtn.setBackgroundResource(R.drawable.round_green_stroke);
+                sashaBtn.setBackgroundResource(R.drawable.white_button_background);
+                nikitaBtn.setBackgroundResource(R.drawable.white_button_background);
+                maxBtn.setBackgroundTintList(null);
             }
         });
 
-        developer3 = (Button) findViewById(R.id.developerMax);
-
-        developer3.setOnClickListener(new View.OnClickListener() {
+        sashaBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this,
                         R.string.developer3,
                         Toast.LENGTH_SHORT).show();
+                sashaBtn.setBackgroundResource(R.drawable.round_green_stroke);
+                maxBtn.setBackgroundResource(R.drawable.white_button_background);
+                nikitaBtn.setBackgroundResource(R.drawable.white_button_background);
+                sashaBtn.setBackgroundTintList(null);
             }
         });
-
 
     }
 
